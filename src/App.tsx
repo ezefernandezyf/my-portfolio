4c0241444570708af8896b00f48a936a5abbcb06
Ezefernandezyf <ezefernandezyf@gmail.com>
2026-02-19 22:20:52 -0300

    chore: update project configuration and dependencies, improve code formatting, and remove unused assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 function App() {
- 
   return (
     <>
-      <h1 className="text-5xl text-blue-300">
-    Hello world!
-  </h1>
+      <h1 className="text-5xl text-blue-300">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
